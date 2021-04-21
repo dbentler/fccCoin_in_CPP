0f4b4ef63356bb4035a73536b52279383d7f9747
dbentler <47250652+dbentler@users.noreply.github.com>
2021-04-21 18:04:06 -0400

    Set up function prototypes in Blockchain.h

--- src/BlockChain.h
+++ src/BlockChain.h
@@ -1,3 +1,29 @@
-class BlockChain{
+#ifndef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
 
+#include "Block.h"
+
+class BlockChain{
+    public:
+        BlockChain();
+        
+        //Constructs Initial Block
+        Block constructGenesis();
+
+        //Constructs and adds new block to chain.
+        Block constructBlock(const int& proofNum, const std::string& prevHash);
+
+        //Checks whether or not the Blockchain is valid
+        static void checkValidity();
+
+        //Adds a new transaction to the data of the transaction
+        void newData();
+
+        // Protects blockchain from attack
+        static bool constructProofOfWork();
+
+        //Returns last block in the chain
+        Block lastBlock();
 };
+
+#endif
